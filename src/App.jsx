3c0e69e9023a6453374ef887d86a5f4aae52ed6f
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2023-12-30 16:23:36 +0600

    daisy ui integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'react-daisyui';
 
 const App = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold underline'>Hello</h1>
+            <Button color='primary'>Click me</Button>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,34 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0090d4",
 
+          secondary: "#007f00",
+
+          accent: "#00ad66",
+
+          neutral: "#1a1304",
+
+          "base-100": "#fff4f3",
+
+          info: "#008bec",
+
+          success: "#38eb2d",
+
+          warning: "#f78d00",
+
+          error: "#c80032",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
